e8e1e1f88cd63928759107a82478d48ac4a67e24
Iván Herrera González <ivanherreragonza@gmail.com>
2025-10-28 13:58:33 +0100

    ultimo push

---
+++ src/views/index.js
@@ -0,0 +1,18 @@
+// src/views/index.js
+import home from './home.js';
+import sobre from './sobre.js';
+import proyectos from './proyectos.js';
+import contacto from './contacto.js';
+import tipografia from './tipografia.js'; // Añade esto
+import notFound from './404.js';
+import componentes from './componentes.js';
+
+export const views = {
+	'/': home,
+	'/sobre': sobre,
+	'/proyectos': proyectos,
+	'/contacto': contacto,
+	'/tipografia': tipografia, 
+    '/componentes': componentes, 
+	404: notFound,
+};
