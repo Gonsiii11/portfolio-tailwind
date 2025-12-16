6c2f992054faf3c05f0577b2ec24673d676a5663
Iván Herrera González <ivanherreragonza@gmail.com>
2025-12-16 14:00:09 +0100

    cambiada las rutas de index.js

--- src/views/index.js
+++ src/views/index.js
@@ -1,11 +1,14 @@
 // demo/src/views/index.js
+
+const BASE_URL = import.meta.env.BASE_URL
+
 export const views = {
-	'/': { templateId: 'view-home', templateUrl: './src/views/home.html' },
-	'/about': { templateId: 'view-about', templateUrl: './src/views/about.html' },
-	'/projects': { templateId: 'view-projects', templateUrl: './src/views/projects.html' },
-	'/contact': { templateId: 'view-contact', templateUrl: './src/views/contact.html' },
-	'/component': { templateId: 'view-component', templateUrl: './src/views/component.html' },
-	'/tipografy': { templateId: 'view-tipografy', templateUrl: './src/views/tipografy.html' },
-	'/buttons': { templateId: 'view-buttons', templateUrl: './src/views/buttons.html' },
-	404: { templateId: 'view-404', templateUrl: './src/views/404.html' },
+	'/': { templateId: 'view-home', templateUrl:  `${BASE_URL}src/views/home.html` },
+	'/about': { templateId: 'view-about', templateUrl: `${BASE_URL}src/views/about.html` },
+	'/projects': { templateId: 'view-projects', templateUrl: `${BASE_URL}src/views/projects.html` },
+	'/contact': { templateId: 'view-contact', templateUrl: `${BASE_URL}src/views/contact.html` },
+	'/component': { templateId: 'view-component', templateUrl: `${BASE_URL}src/views/component.html` },
+	'/tipografy': { templateId: 'view-tipografy', templateUrl: `${BASE_URL}src/views/tipografy.html`},
+	'/buttons': { templateId: 'view-buttons', templateUrl: `${BASE_URL}src/views/buttons.html`},
+	404: { templateId: 'view-404', templateUrl: `${BASE_URL}src/views/404.html` },
 };
